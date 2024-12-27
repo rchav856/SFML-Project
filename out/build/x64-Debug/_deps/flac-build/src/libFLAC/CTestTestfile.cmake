c264b41cb2127439dab3f0cb7c111391b2f1035c
randy <rchavarria856@gmail.com>
2024-12-27 17:11:26 -0500

    made changes to .gitignore to allow certain files over

---
+++ out/build/x64-Debug/_deps/flac-build/src/libFLAC/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/rchav/source/repos/SFML-Project/out/build/x64-Debug/_deps/flac-src/src/libFLAC
+# Build directory: C:/Users/rchav/source/repos/SFML-Project/out/build/x64-Debug/_deps/flac-build/src/libFLAC
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
