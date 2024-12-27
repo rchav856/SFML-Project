a266ecacc4c530cef9e0c887a6d4b2e70a26e538
rchav856 <160417077+rchav856@users.noreply.github.com>
2024-12-27 17:50:59 -0500

    Delete out/build/x64-Debug directory
    giving issues with visual studio

--- out/build/x64-Debug/_deps/flac-build/src/libFLAC/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/rchav/source/repos/SFML-Project/out/build/x64-Debug/_deps/flac-src/src/libFLAC
-# Build directory: C:/Users/rchav/source/repos/SFML-Project/out/build/x64-Debug/_deps/flac-build/src/libFLAC
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
