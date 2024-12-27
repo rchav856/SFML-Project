4af147f9fa28f8bb0d85b2c1139daad449b2c571
randy <rchavarria856@gmail.com>
2024-12-27 16:24:50 -0500

    Now has a movable sprite ball, starts position at middle of scene

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,56 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
+
+void keyboardMovements(sf::Sprite&);
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
-
+    // resolution
+    const auto SCREEN_WIDTH = 1600, SCREEN_HEIGHT = 800;
+    // Main Window Creation
+    sf::RenderWindow window(sf::VideoMode({ SCREEN_WIDTH, SCREEN_HEIGHT }), "SillyGame", sf::State::Windowed);
+    window.setMouseCursorVisible(1);
+    window.setFramerateLimit(60);
+    // Get ball photo
+    sf::Texture blackBallTexture("assets/chars/BlackBall.png");
+    // Load ball into sprite
+    sf::Sprite ballUser(blackBallTexture);
+    ballUser.setTextureRect({ sf::IntRect({614, 334}, {50,50}) }); // grab ball picture
+    ballUser.setOrigin(ballUser.getGlobalBounds().getCenter());
+    // Set the sprite's position
+    ballUser.setPosition({SCREEN_WIDTH/2.f, SCREEN_HEIGHT/2.f});
+    ballUser.setScale({ .8f, .8f });
+    
+    
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
         {
             if (event->is<sf::Event::Closed>())
-            {
                 window.close();
-            }
         }
-
-        window.clear();
+        window.clear(sf::Color::White);
+        window.draw(ballUser);
         window.display();
+        keyboardMovements(ballUser);
     }
-    return 0;
 }
+
+// Handles movement of any sprite based on user keyboard inputs
+void keyboardMovements(sf::Sprite& ballUser) {
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left)) {
+        ballUser.move({ -5,0 });
+        ballUser.rotate(sf::degrees(-5));
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right)) {
+        ballUser.move({ 5,0 });
+        ballUser.rotate(sf::degrees(5));
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up)) {
+        ballUser.move({ 0, -5 });
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down)) {
+        ballUser.move({ 0,5 });
+     }
+ }
